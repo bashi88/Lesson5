54f3989d4df7c049c3526fe7ce312202b8e211f9
unknown <sienk001@D0117108.wurnet.nl>
2015-01-09 17:48:37 +0100

    basic output functions and visualizations need adjustments

--- Main.R
+++ Main.R
@@ -3,6 +3,7 @@
 # Date: 08/01/2015       
 # Exercise 5
 
+<<<<<<< HEAD
 library(raster)
 library(sp)
 library(rgdal)
@@ -46,3 +47,12 @@ placesRD@data[locationinfo]
 
 # name of intersecting city: Utrecht
 # population of intersecting city: 100,000
+
+
+# Selects the “industrial” (type == "industrial") railways
+# Buffers the “industrial” railways with a buffer of 1000m (hint: gBuffer with byid=TRUE)
+# Find the place (i.e. a city) that intersects with this buffer.
+# Create a plot that shows the buffer, the points, and the name of the city
+# write down the name of the city and the population of that city as one comment at the end of the script.
+# Bonus (optional!): if you can also download and unzip this within the script.
+
