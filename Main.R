f3f0bceec039b94b49ae9de7e64fcb6a139cb722
Barbara <barbasien@wp.pl>
2015-01-09 14:12:15 +0100

    tips added

--- Main.R
+++ Main.R
@@ -2,3 +2,10 @@
 # Team Members: Roeland de Koning / Barbara Sienkiewicz    
 # Date: 08/01/2015       
 # Exercise 5
+
+# Selects the “industrial” (type == "industrial") railways
+# Buffers the “industrial” railways with a buffer of 1000m (hint: gBuffer with byid=TRUE)
+# Find the place (i.e. a city) that intersects with this buffer.
+# Create a plot that shows the buffer, the points, and the name of the city
+# write down the name of the city and the population of that city as one comment at the end of the script.
+# Bonus (optional!): if you can also download and unzip this within the script.
